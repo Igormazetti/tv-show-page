019fecfa48f417628dac143eb43f4e6b76214b9c
Igor Mazetti <igor.mazetti@logithink.it>
2025-10-30 09:21:40 -0300

    refactor: simplify cast section to text-only carousel
    - Remove images from cast cards
    - Display only actor name and character placeholder
    - Convert to horizontal scrolling carousel layout
    - Add smooth scroll behavior
    - Center text alignment within cards
    - Match design for cast display

--- src/js/components/CastSection.js
+++ src/js/components/CastSection.js
@@ -11,8 +11,8 @@ export class CastSection {
       classes: 'cast-section'
     });
 
-    const grid = createElement('div', {
-      classes: 'cast-section__grid'
+    const carousel = createElement('div', {
+      classes: 'cast-section__carousel'
     });
 
     this.cast.forEach(member => {
@@ -21,23 +21,14 @@ export class CastSection {
       });
 
       card.innerHTML = `
-        <div class="cast-card__image">
-          <div class="cast-card__placeholder">
-            <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" fill="currentColor"/>
-            </svg>
-          </div>
-        </div>
-        <div class="cast-card__content">
-          <h4 class="cast-card__name">${member.Name}</h4>
-          <p class="cast-card__role">Ator principal</p>
-        </div>
+        <h4 class="cast-card__name">${member.Name}</h4>
+        <p class="cast-card__character">Personagem</p>
       `;
 
-      grid.appendChild(card);
+      carousel.appendChild(card);
     });
 
-    this.element.appendChild(grid);
+    this.element.appendChild(carousel);
 
     return this.element;
   }
